d7a668560ac1cd17299bcecb43cf2945c21b3b92
suyashrooongta <41545593+suyashrooongta@users.noreply.github.com>
2025-03-23 10:38:16 +0530

    Update FramedImage.tsx

--- src/FramedImage.tsx
+++ src/FramedImage.tsx
@@ -2,29 +2,30 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const FancyFramedImage = ({ imageUrl, altText }) => {
+const FancyFramedImage = ({ imageUrl, altText, onLoad }) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const img = new Image();
     img.onload = () => {
       setIsLoaded(true);
+      onLoad();
     };
     img.src = imageUrl;
-  }, [imageUrl]);
+  }, [imageUrl, onLoad]);
 
   return (
     <AnimatePresence>
       {isLoaded && (
         <motion.img
-          key={imageUrl} // Important for AnimatePresence
+          key={imageUrl}
           src={imageUrl}
           alt={altText}
           className="fancy-framed-image max-w-3xl rounded-lg shadow-xl"
-          initial={{ opacity: 0 }} // Initial animation state
-          animate={{ opacity: 1 }} // Animation when loaded
-          exit={{ opacity: 0 }} // Animation when unmounted
-          transition={{ duration: 1.5 }} // Animation duration
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1.5 }}
           style={{
             maxWidth: '80vw',
             maxHeight: '75vh',
